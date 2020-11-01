016be5e6e3cb5b4a57a34956db311a9053dcc859
Kayla Weselowski <kweselowski@gmail.com>
2020-11-01 02:30:19 -0600

    form and flexbox

--- app.js
+++ app.js
@@ -3,14 +3,8 @@ window.onload = init();
 function init(){
     let links = document.getElementsByClassName('link');
     for (let l of links){
-        l.addEventListener('mouseover', function(e){
-            e.target.style.color = "magenta";
-        });
-        l.addEventListener('mouseout', function(e){
-            e.target.style.color = "#300c94";
-        });
         l.addEventListener('click', function(e){
-            e.target.style.color = "green";
+            e.target.style.color = "#810292";
         });
     }
 
