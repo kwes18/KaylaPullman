64f712faf622a817a136852e3218d403c929e88c
Kayla Weselowski <kweselowski@gmail.com>
2020-12-10 01:21:27 -0600

    style changes
    landing and home

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 window.onload = init();
 
 function init() {
-    let links = document.getElementsByClassName('link');
-    for (let l of links) {
-        l.addEventListener('click', function(e) {
-            e.target.style.color = "#810292";
-        });
 
-        document.getElementById("enter").addEventListener("click", )
 
-    }
 
 }
