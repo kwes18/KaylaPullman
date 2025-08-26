c75cead1bf9d6e2a229ba5e5aff54d7e0d2e8eaa
Kayla Pullman <43488855+kwes18@users.noreply.github.com>
2025-08-26 17:28:43 -0500

    copyright, link typo
    project link typo, dynamic year

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 window.onload = init();
 
 function init() {
+  let btnContainer = document.getElementById("navbar");
 
-let btnContainer = document.getElementById("navbar");
+  let links = btnContainer.getElementsByClassName("link");
 
-let links = btnContainer.getElementsByClassName("link");
+  let copyrightYear = document.getElementById("copyright-year");
 
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", ()=> {
-    let current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", () => {
+      let current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+  const currentYear = new Date().getFullYear();
+  copyrightYear.innerHTML =
+    "All rights reserved Kayla Pullman " + currentYear + " &copy;";
 }
